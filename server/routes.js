a83ede8a1963b8e026c2815571896ccef7f4af8f
Nolan Schneider <ngschneider@unomaha.edu>
2020-03-08 09:43:27 -0500

    asdf

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    res.send("<center>asdfasdfa<h1>x = (-b+-sqrt(b^2-4ac)/2a)</h1><br>🍑</center>");
+router.get('/where', (req,res) => {
+    res.send("{des moines}");
 
 });
 
